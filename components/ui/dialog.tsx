f891218f26e3a69fa3e546bca89e657d54d9c55d
soporte-efc <jcontreras@efc.com.pe>
2026-02-02 21:31:35 -0500

    Añadir DialogFooter al componente dialog para fix build Vercel

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -85,6 +85,20 @@ const DialogDescription = React.forwardRef<
 ));
 DialogDescription.displayName = DialogPrimitive.Description.displayName;
 
+const DialogFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:gap-2",
+      className
+    )}
+    {...props}
+  />
+);
+DialogFooter.displayName = "DialogFooter";
+
 export {
   Dialog,
   DialogPortal,
@@ -95,4 +109,5 @@ export {
   DialogHeader,
   DialogTitle,
   DialogDescription,
+  DialogFooter,
 };
